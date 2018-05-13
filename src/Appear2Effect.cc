72d1de8cd62e3ac5cda1de3d41045256280f4376
Vlad Zagorodniy <vladzzag@gmail.com>
2018-05-13 23:55:25 +0300

    Fix perspective distortions
    Perspective projection distorts objects near edges of the viewport too
    much. Fix this problem by moving the center of windows to origin.

--- src/Appear2Effect.cc
+++ src/Appear2Effect.cc
@@ -20,6 +20,12 @@
 // KConfigSkeleton
 #include "appear2config.h"
 
+// kwineffects
+#include <kwinglutils.h>
+
+// Qt
+#include <QMatrix4x4>
+
 Appear2Effect::Appear2Effect()
 {
     initConfig<Appear2Config>();
@@ -87,6 +93,28 @@ void Appear2Effect::paintWindow(KWin::EffectWindow* w, int mask, QRegion region,
     auto it = m_animations.constFind(w);
     if (it != m_animations.cend()) {
         const qreal t = (*it).value();
+
+        const QRectF screenGeo = KWin::GLRenderTarget::virtualScreenGeometry();
+        const QRectF windowGeo = w->geometry();
+
+        // Perspective projection distorts objects near edges
+        // of the viewport. This is critical because distortions
+        // near edges of the viewport are not desired with this effect.
+        // To fix this, the center of the window will be moved to the origin,
+        // after applying perspective projection, the center is moved back
+        // to its "original" projected position. Overall, this is how the window
+        // will be transformed:
+        //  [move to the origin] -> [rotate] -> [translate] ->
+        //    -> [perspective projection] -> [reverse "move to the origin"]
+        const QMatrix4x4 oldProjMatrix = data.screenProjectionMatrix();
+        const QVector3D invOffset = oldProjMatrix.map(QVector3D(windowGeo.center()));
+        QMatrix4x4 invOffsetMatrix;
+        invOffsetMatrix.translate(invOffset.x(), invOffset.y());
+        data.setProjectionMatrix(invOffsetMatrix * oldProjMatrix);
+
+        // Move the center of the window to the origin.
+        const QPointF offset = screenGeo.center() - windowGeo.center();
+        data.translate(offset.x(), offset.y());
 
         data.setRotationAxis(Qt::XAxis);
         data.setRotationAngle(interpolate(m_pitch, 0, t));
